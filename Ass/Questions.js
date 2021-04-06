53d5d1ed4bb497ade4f14861682861d21d56e06f
Ronaldo004 <50299293+Ronaldo004@users.noreply.github.com>
2021-04-06 17:04:28 +0500

    i done

---
+++ Ass/Questions.js
@@ -0,0 +1,121 @@
+// //========================CHAPTER 21 TO 25========================
+
+// QUESTION 1:
+
+// let firstName= prompt("Enter First Name : ");
+// let lastName= prompt("Enter Last Name : ");
+// let fullName=firstName + lastName ;
+
+// document.write(fullName);
+
+
+
+
+
+//QUESTION 2:
+//  var a = prompt("Enter String/Text");
+//  document.write("<br/>"+"User given string: "+ string+ "<br/>"+"Length of user given string is: " + a.length);
+
+//QUESTION 3:
+// // var b = "pakistani";
+// // document.write("<br>" + "Index of 'n' is: " + b.indexOf("n"));
+
+
+//QUESTION 4:
+// // var a = "Hello World";
+// // document.write("<br/>Last index of string: " + a.lastIndexOf("l"));
+
+// QUESTION 5:
+// // var c = "Pakistani";
+// // document.write("<br/>"+ "Character at index 3: " + c.charAt(3));
+
+//QUESTION 6:
+// // var a = prompt("Enter your first name:");
+// // var b = prompt("Enter your last name:");
+// // let fullName = firstName.concat(" ",b);
+// // document.write("<br>" + "Full Name using string concat function: " + fullName);
+
+//QUESTION 7:
+// // var k = "Hyderabad";
+// // document.write("<br>" + city.replace("Hydera","faisal"));
+
+// QUESTION 8:
+// // var lil = "Rohan and Sheryar are best friends. They play Snooker and cricker together.";
+// // document.write(message.replace(/and/g,"&"));
+
+//QUESTION 9:
+// var str = "472";
+// document.write("<br>"+ "Data type of str: " + typeof str);
+//  str = Number(str);
+// document.write("<br>"+ "Data type of str: " + typeof str);
+
+// QUESTION 10:
+//  var f = prompt("Enter any fruit");
+// document.write("<br>"+ "User input : " + f);
+//document.write("<br>"+ "Upper Case : " + f.toUpperCase());
+
+// QUESTION 11:
+//  var f = prompt("Enter any fruit");
+//  document.write("<br>"+ "User input : " + f);
+//  document.write("<br>"+ "Lower Case : " + f.toLowerCase());
+
+// QUESTION 12:
+//  var num = 34.32;
+//  number = num.toString();
+//  document.write("<br>" + "Number: " + number);
+// var Res = number.replace(".","");
+// document.write("<br>" + "Result: " + Res);
+
+// QUESTION 13:
+//  var userName = prompt("Enter user name ");
+//  for (let i = 0; i < userName.length; i++) {
+//     if (userName[i] === "@" || userName[i] === "," || userName[i] === "." || userName[i] === "!") {
+//          alert("Please Enter a valid username");
+//    break;
+// }
+// }
+
+// QUESTION 14:
+//  var sweets = ["cake", "Gulab jamun", "Pastires", "Brownie", "patties"];
+//  var order = prompt("Enter your order");
+//  var index = sweets.indexOf(order.toLowerCase());
+//  if(index === -1){
+//      document.write("<br>" + "We are sorry." + order + " is not avalible in our bakery");
+//  }
+//  else{
+//      document.write("<br>"+ order.toLowerCase() + " is avalible at index " + index + " in our bakery");
+// }
+
+
+//QUESTION 16:
+// var university = "SSUET";
+//  var arr = university.split("");
+
+//  let i = 0;
+
+// while(i<university.length){
+//     document.write("<br>"+array[i]);
+//    i++;
+//  }
+
+// QUESTION 17:
+//  var userInput = prompt("Enter any thing");
+//  var lastIndex = userInput.length;
+//  document.write("<br>"+"User input: " + userInput);
+//  document.write("<br>"+ "Last character of user input: " + userInput[lastIndex-1]);
+
+// QUESTION 18:
+// var b = "The quick brown fox jumps over the lazy dog";
+// var i= 0;
+// var count = 0;
+// while(i < b.length){
+//  if("the" === (b.slice(i,i+3).toLowerCase())){
+//         count++;
+//   }
+//  i++;
+// }
+// document.write("<br>"+ "Text: " + b);
+// document.write("<br>"+ "There are "+ count + " occurance(s) of the word \"the\"");
+
+// // =================================complete
+
